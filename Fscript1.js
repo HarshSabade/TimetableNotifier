d659e1f460421b273c6a55febc3ad97d2478d157
HarshSabade <harshsabade34637@gmail.com>
2024-02-24 20:56:06 +0530

    Ready for Monday trial

--- Fscript1.js
+++ Fscript1.js
@@ -4,16 +4,19 @@ function day() {
             function give() {
                   var interval = setInterval(function () {
                         var currentDate = new Date();
-                        if (currentDate.getDay() == 6 && currentDate.getHours() == 16)
+                        if (currentDate.getDay() == 1 && currentDate.getHours() == 9)
                               window.location.href = "monday f1.html"
-                        else if (currentDate.getDay() == 4 && currentDate.getHours() == 16) {
+                        else if (currentDate.getDay() == 1 && currentDate.getHours() == 10)
                               window.location.href = "monday f2.html"
-                        }
-                        else if (currentDate.getDay() == 4 && currentDate.getHours() == 16)
+                        else if (currentDate.getDay() == 1 && currentDate.getHours() == 11 && currentDate.getMinutes() < 11)
                               window.location.href = "monday f3.html"
-                        else if (currentDate.getDay() == 4 && currentDate.getHours() == 16)
+                        else if (currentDate.getDay() == 1 && currentDate.getHours() == 11 && currentDate.getMinutes() >= 10 || currentDate.getHours() == 12 && currentDate.getMinutes() < 11)
                               window.location.href = "monday f4.html"
-                        else if (currentDate.getDay() == 4 && currentDate.getHours() == 16)
+                        else if (currentDate.getDay() == 1 && currentDate.getHours() == 12 && currentDate.getMinutes() >= 10)
+                              window.location.href = "monday f5.html"
+                        else if (currentDate.getDay() == 1 && currentDate.getHours() == 1 || currentDate.getHours() == 2)
+                              window.location.href = "monday f6.html"
+                        else if (currentDate.getDay() == 0 || currentDate.getDay() == 1 && currentDate.getHours() < 9)
                               window.location.href = "upcoming mon.html"
                         else (
                               window.location.href = "nolec.html"
