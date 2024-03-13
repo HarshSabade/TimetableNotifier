1301e48063cb3d84c0b113fde08f831587fd1abb
HarshSabade <harshsabade34637@gmail.com>
2024-03-14 04:08:49 +0530

    Webpages for F div Friday added and added to JS logic (thursday upcoming does not seem to be working in the previous version)

--- Fscript.js
+++ Fscript.js
@@ -56,7 +56,7 @@ function day() {
                         else if (currentDate.getDay() == 3 && currentDate.getHours() == 15 || currentDate.getHours() == 16)
                               window.location.href = "wednesday f7.html"
                         // For Thursday
-                        else if (currentDate.getDay() == 4 && currentDate.getHours() <= 11 && currentDate.getMinutes() < 11)
+                        else if (currentDate.getDay() == 4 && currentDate.getHours() < 12 && currentDate.getMinutes() < 11)
                               window.location.href = "upcoming thu.html"
                         else if (currentDate.getDay() == 4 && currentDate.getHours() == 11 && currentDate.getMinutes() >= 10 || currentDate.getHours() == 12 && currentDate.getMinutes() < 11)
                               window.location.href = "thursday f1.html"
@@ -66,6 +66,21 @@ function day() {
                               window.location.href = "thursday f3.html"
                         else if (currentDate.getDay() == 4 && currentDate.getHours() == 15 || currentDate.getHours() == 16)
                               window.location.href = "thursday f4.html"
+                        // For Friday
+                        else if (currentDate.getDay() == 5 && currentDate.getHours() < 9)
+                              window.location.href = "upcoming fri.html"
+                        else if (currentDate.getDay() == 5 && currentDate.getHours() == 9 || currentDate.getHours() == 10)
+                              window.location.href = "friday f1.html"
+                        else if (currentDate.getDay() == 5 && currentDate.getHours() == 11 && currentDate.getMinutes() < 11)
+                              window.location.href = "friday f2.html"
+                        else if (currentDate.getDay() == 5 && currentDate.getHours() == 11 && currentDate.getMinutes() >= 10 || currentDate.getHours() == 12 || currentDate.getHours() == 13 && currentDate.getMinutes() < 11)
+                              window.location.href = "friday f3.html"
+                        else if (currentDate.getDay() == 5 && currentDate.getHours() == 13 && currentDate.getMinutes() >= 10)
+                              window.location.href = "friday f4.html"
+                        else if (currentDate.getDay() == 5 && currentDate.getHours() == 14)
+                              window.location.href = "friday f5.html"
+                        else if (currentDate.getDay() == 5 && currentDate.getHours() == 15)
+                              window.location.href = "friday f6.html"
                         else (
                               window.location.href = "nolec.html"
                         )
