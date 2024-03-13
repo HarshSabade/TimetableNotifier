4d43f85e75e2fad7675e8f87753a35d87fdaa545
HarshSabade <harshsabade34637@gmail.com>
2024-03-13 20:13:15 +0530

    Jap/Ger webpage added and added to js logic Timetable for monday complete

--- Fscript.js
+++ Fscript.js
@@ -4,7 +4,9 @@ function day() {
             function give() {
                   var interval = setInterval(function () {
                         var currentDate = new Date();
-                        if (currentDate.getDay() == 1 && currentDate.getHours() == 9)
+                        if (currentDate.getDay() == 1 && currentDate.getHours() < 9)
+                              window.location.href = "upcoming mon.html"
+                        else if (currentDate.getDay() == 1 && currentDate.getHours() == 9)
                               window.location.href = "monday f1.html"
                         else if (currentDate.getDay() == 1 && currentDate.getHours() == 10)
                               window.location.href = "monday f2.html"
@@ -16,8 +18,8 @@ function day() {
                               window.location.href = "monday f5.html"
                         else if (currentDate.getDay() == 1 && currentDate.getHours() == 1 || currentDate.getHours() == 2)
                               window.location.href = "monday f6.html"
-                        else if (currentDate.getDay() == 1 && currentDate.getHours() < 9)
-                              window.location.href = "upcoming mon.html"
+                        else if (currentDate.getDay() == 1 && currentDate.getHours() == 3 || currentDate.getHours() == 4)
+                              window.location.href = "monday f7.html"
                         else (
                               window.location.href = "nolec.html"
                         )
--- Fscript1.js
+++ Fscript1.js
@@ -4,7 +4,9 @@ function day() {
             function give() {
                   var interval = setInterval(function () {
                         var currentDate = new Date();
-                        if (currentDate.getDay() == 1 && currentDate.getHours() == 9)
+                        if (currentDate.getDay() == 1 && currentDate.getHours() < 9)
+                              window.location.href = "upcoming mon.html"
+                        else if (currentDate.getDay() == 1 && currentDate.getHours() == 9)
                               window.location.href = "monday f1.html"
                         else if (currentDate.getDay() == 1 && currentDate.getHours() == 10)
                               window.location.href = "monday f2.html"
@@ -16,15 +18,21 @@ function day() {
                               window.location.href = "monday f5.html"
                         else if (currentDate.getDay() == 1 && currentDate.getHours() == 1 || currentDate.getHours() == 2)
                               window.location.href = "monday f6.html"
-                        else if (currentDate.getDay() == 1 && currentDate.getHours() < 9)
-                              window.location.href = "upcoming mon.html"
+                        else if (currentDate.getDay() == 1 && currentDate.getHours() == 3 || currentDate.getHours() == 4)
+                              window.location.href = "monday f7.html"
                         else (
                               window.location.href = "nolec.html"
                         )
-                  }, 2000);
+                  }, 5000);
             }
             give()
-            // function give1() {
+      }
+      else {
+            console.log("Not my division")
+      }
+      console.log("hi");
+}
+// function give1() {
             //       var timeout = setTimeout(function () {
             //             var currentDate = new Date();
             //             if (currentDate.getDay() == 1 && currentDate.getHours() == 9)
@@ -48,13 +56,6 @@ function day() {
             // }
 
             // window.onload = give1()
-      }
-      else {
-            console.log("Not my division")
-      }
-      console.log("hi");
-}
-
 // window.onload=day()
 // window.onload=give()
 // window.onload = day()
